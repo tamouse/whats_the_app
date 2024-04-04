69de48ed5e3ff2867cb139111d040727c33f8a9a
Tamara Temple <tamara@mrets.org>
2024-04-04 11:45:38 -0500

    Saved API key in secret config

--- lib/whats_the_temp.rb
+++ lib/whats_the_temp.rb
@@ -1,8 +1,17 @@
 # frozen_string_literal: true
 
+require 'dry/credentials'
 require_relative "whats_the_temp/version"
 
+
 module WhatsTheTemp
+  extend Dry::Credentials
+
+  credentials do
+    env "sandbox"
+    dir "."
+  end
+
   class Error < StandardError; end
   # Your code goes here...
 end
