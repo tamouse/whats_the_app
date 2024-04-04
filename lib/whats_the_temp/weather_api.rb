ff235af2d48c8973f4f6f016bb69546b20a61696
Tamara Temple <tamara@mrets.org>
2024-04-04 13:40:53 -0500

    Get the current temp

--- lib/whats_the_temp/weather_api.rb
+++ lib/whats_the_temp/weather_api.rb
@@ -3,18 +3,30 @@
 # lib/weather_api.rb
 
 require "uri"
+require "net/http"
+require "json"
 
 module WhatsTheTemp
   # Client for the weatherapi.com API
   class WeatherApi
     API_KEY = "d83c18de62814e559d1143220240404" # Normally, you should put this into an encrypted file
-    BASE_URL = "http://api.weatherapi.com/v1"
+    BASE_URL = "http://api.weatherapi.com/v1/current.json"
+
+    def current_temp_for(address)
+      weather = get_weather_for(address)
+      weather.dig("current", "temp_f")
+    end
+
+    def get_weather_for(address)
+      url = create_url(address)
+      JSON.parse(Net::HTTP.get(url))
+    end
 
     def create_url(query)
       uri = URI(BASE_URL)
-      qs = URI.encode_www_form(key: API_KEY, q: query)
+      qs = URI.encode_www_form(key: API_KEY, q: query, aqi: "no")
       uri.query = qs
-      uri.to_s
+      uri
     end
   end
 end
--- test/whats_the_temp/test_weather_api.rb
+++ test/whats_the_temp/test_weather_api.rb
@@ -3,13 +3,34 @@
 # weather_ap_test file for weather_api
 
 require "test_helper"
+require "json"
 
 module WhatsTheTemp
   class TestWeatherApi < Minitest::Test
-    def test_create_url
-      w = WhatsTheTemp::WeatherApi.new
+    def setup
+      @w = WhatsTheTemp::WeatherApi.new
+      @zip = "55118" # St. Paul, MN
+    end
 
-      assert_equal "http://api.weatherapi.com/v1?key=d83c18de62814e559d1143220240404&q=55118", w.create_url("55118")
+    def test_current_temp_for
+      weather = @w.current_temp_for(@zip)
+      # puts weather.inspect
+
+      refute_nil weather
+    end
+
+    def test_create_url
+      # assert_equal "http://api.weatherapi.com/v1/current.json?key=d83c18de62814e559d1143220240404&q=55118&aqi=no",
+      assert_equal  "http://api.weatherapi.com/v1/current.json?key=d83c18de62814e559d1143220240404&q=55118&aqi=no",
+                    @w.create_url(@zip).to_s
+    end
+
+    def test_get_weather_for
+      response = @w.get_weather_for @zip
+
+      refute_nil response
+
+      assert_equal "Saint Paul", response.dig("location", "name")
     end
   end
 end
