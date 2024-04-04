8f9ee9f931c3d933ecfce258582cff29429d30a8
Tamara Temple <tamara@mrets.org>
2024-04-04 10:31:21 -0500

    initial commit
    - ran `bundle gem ...`
    - ran `bundle install`

---
+++ lib/whats_the_temp.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "whats_the_temp/version"
+
+module WhatsTheTemp
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "whats_the_temp"
+
+require "minitest/autorun"
