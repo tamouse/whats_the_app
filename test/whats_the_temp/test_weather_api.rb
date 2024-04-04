868977189728b11f47eb9b219d19a06175c87399
Tamara Temple <tamara@mrets.org>
2024-04-04 16:01:36 -0500

    Add VCR gem to reduce endpoint calls
    - installed VCR gem in Gemfile (testing only)
    - installed WebMock gem to work with VCR
    
    These should only be needed for the Weather API

--- test/whats_the_temp/test_weather_api.rb
+++ test/whats_the_temp/test_weather_api.rb
@@ -4,19 +4,25 @@
 
 require "test_helper"
 require "json"
+require "vcr"
 
 module WhatsTheTemp
   class TestWeatherApi < Minitest::Test
     def setup
       @w = WhatsTheTemp::WeatherApi.new
       @zip = "55118" # St. Paul, MN
+
+      VCR.configure do |c|
+        c.cassette_library_dir = "test/fixtures/vcr_cassettes"
+        c.hook_into :webmock
+      end
     end
 
     def test_current_temp_for
-      weather = @w.current_temp_for(@zip)
-      # puts weather.inspect
-
-      refute_nil weather
+      VCR.use_cassette "current_temp_for" do
+        weather = @w.current_temp_for(@zip)
+        refute_nil weather
+      end
     end
 
     def test_create_url
@@ -25,11 +31,11 @@ module WhatsTheTemp
     end
 
     def test_get_weather_for
-      response = @w.send :get_weather_for, @zip
-
-      refute_nil response
-
-      assert_equal "Saint Paul", response.dig("location", "name")
+      VCR.use_cassette "get_weather_for" do
+        response = @w.send :get_weather_for, @zip
+        refute_nil response
+        assert_equal "Saint Paul", response.dig("location", "name")
+      end
     end
   end
 end
